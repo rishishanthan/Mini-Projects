7f94ad3ffc89dcd90e6017e71975e228f4963a42
Rishi Shanthan Bhagavatham <rishishanthan@gmail.com>
2025-09-18 10:27:35 -0400

    Add Numerical Mathematics first commit

---
+++ Numerical-Mathematics/gauss-newton-regression/rbhagava_midterm_p2.m
@@ -0,0 +1,25 @@
+function [r, J] = rbhagava_midterm_p2(af, x, y)
+%RBHAGAVA_MIDTERM_P2 Matlab function fitting logistic regression model
+%
+% Inputs:
+%   af - The coefficient list.
+%   x - The x-locations.
+%   y - The y-locations.
+%
+% Outputs:
+%   r - The residual vector
+%   J - The Jacobian matrix
+
+    f = @(x)(af(1) / 1 + (exp(af(2) + x * af(3))));
+    r = zeros(length(y), 1);
+    
+    J(:, 1) = 1 ./ (1 + exp(af(3) .* x + af(2)));
+    J(:, 2) = (-(1 ./ (1 + exp(af(2) + af(3) .* x).^2))) .* exp(af(2) + af(3) .* x);
+    J(:, 3) = (-(1 ./ (1 + exp(af(2) + af(3) .* x).^2))) .* exp(af(2) + af(3) .* x) .* x;
+
+    for i = 1:length(af)
+        r(i) = y(i) - f(x(i));
+    end
+    
+end %rbhagava_midterm_p2
+
