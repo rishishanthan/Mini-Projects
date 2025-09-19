67b1983c2cdeaa5472558b20328bb9b144a81adc
Rishi Shanthan Bhagavatham <rishishanthan@gmail.com>
2025-09-19 18:34:29 -0400

    Created small works

---
+++ Numerical-Mathematics/small-works/rbhagava_pp1.m
@@ -0,0 +1,12 @@
+function [b] = rbhagava_pp1(a)
+%RBHAGAVA_PP1 MATLAB function which assigns the value in a to b.
+%
+% Inputs:
+%   a - assign an input in the command line while running the function or while calling the function in the command line. Takes inputs which are fit to a sine function
+%
+% Outputs:
+%   b - is the output of the sine value of variable 'a'
+
+    b = a;
+
+end %rbhagava_pp1
